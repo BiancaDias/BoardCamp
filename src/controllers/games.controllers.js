d0dfd6ebaeed61d37ca125ab477e84162dc02ea7
Bianca Dias <bianca.dias@ufv.br>
2023-05-10 19:24:52 -0300

    feat: rota post/games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 export async function getGames(req, res){
     try{
         const games = await db.query(`SELECT * FROM games;`)
-        res.send(games)
+        res.send(games.rows)
     }catch (err) {
         res.status(500).send(err.message)
     }
@@ -13,7 +13,8 @@ export async function postGames(req, res){
     const { name, image, stockTotal, pricePerDay } = req.body;
 
     try{
-        const insertgame = await db.query("")
+        insertgame = await db.query("INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3);" [name, image, stockTotal, pricePerDay])
+        res.sendStatus(201)
     }catch (err) {
         res.status(500).send(err.message)
     }
