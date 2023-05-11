f14735966087c95aa4e6050d773ba2afa6f78b43
Bianca Dias <bianca.dias@ufv.br>
2023-05-11 11:21:12 -0300

    feat: validação de dados dos jogos

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -13,7 +13,12 @@ export async function postGames(req, res){
     const { name, image, stockTotal, pricePerDay } = req.body;
 
     try{
-        insertgame = await db.query("INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3);" [name, image, stockTotal, pricePerDay])
+        const verGame = await db.query(`SELECT * FROM games WHERE name = '${name}';`)
+        if(verGame.rowCount > 0){
+            console.log(verGame)
+            return res.sendStatus(409);
+        }
+        await db.query("INSERT INTO games (name, image, \"stockTotal\", \"pricePerDay\") VALUES ($1, $2, $3, $4);", [name, image, stockTotal, pricePerDay]);
         res.sendStatus(201)
     }catch (err) {
         res.status(500).send(err.message)
