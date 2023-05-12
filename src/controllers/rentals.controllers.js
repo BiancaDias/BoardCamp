08ce57deb6346642823a9f79ceb4995bd3bdbe57
Bianca Dias <bianca.dias@ufv.br>
2023-05-12 10:21:15 -0300

    feat: rota delete /rentals/:id

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -64,3 +64,20 @@ export async function postRentalsReturn(req, res){
         res.status(500).send(err.message)
     }
 }
+
+export async function deleteRentals(req, res){
+    const { id } = req.params;
+    try{
+        const rental = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id])
+        if(rental.rowCount === 0){
+            return res.sendStatus(404);
+        }
+        if(rental.rows[0].delayFee === null){
+            return res.sendStatus(400)
+        }
+        await db.query(`DELETE FROM rentals WHERE id = $1`, [id]);
+        res.sendStatus(200)
+    }catch (err) {
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { postRentals, postRentalsReturn } from "../controllers/rentals.controllers.js";
+import { deleteRentals, postRentals, postRentalsReturn } from "../controllers/rentals.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { rentalsSchema } from "../schemas/rentals.schemas.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals",validateSchema(rentalsSchema), postRentals);
-rentalsRouter.post("/rentals/:id/return", postRentalsReturn)
+rentalsRouter.post("/rentals/:id/return", postRentalsReturn);
+rentalsRouter.delete("/rentals/:id", deleteRentals);
 
 export default rentalsRouter;
