e37d8b9ca62631736bbd2df9e9e93605ddd50729
Bianca Dias <bianca.dias@ufv.br>
2023-05-12 10:12:29 -0300

    feat: rota post /rentals/:id/return

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -13,7 +13,6 @@ export async function postRentals(req, res){
         if(customer.rowCount === 0){
             return res.sendStatus(400)
         }
-        console.log(game.rows[0].pricePerDay)
 
         const gameRented = await db.query(`SELECT * FROM rentals WHERE "gameId" = $1`, [gameId]);
         if(gameRented.rowCount === game.rows[0].stockTotal){
@@ -34,6 +33,33 @@ export async function postRentals(req, res){
             [customerId, gameId, rentDate, daysRented, null, originalPrice, null]
         )
         res.sendStatus(201)
+    }catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function postRentalsReturn(req, res){
+    const { id } = req.params;
+    const returnDate = dayjs().format('YYYY-MM-DD');
+    const returnDateForCalc = dayjs().startOf('day');
+    try{
+        const rental = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id])
+        if(rental.rowCount === 0){
+            return res.sendStatus(404);
+        }
+        if(rental.rows[0].delayFee !== null){
+            return res.sendStatus(400)
+        }
+        const days = Math.floor((returnDateForCalc.valueOf() - rental.rows[0].rentDate.valueOf())/86400000);
+        let delayFee;
+        if(days > rental.rows[0].daysRented){
+            delayFee = (days - rental.rows[0].daysRented) * (rental.rows[0].originalPrice / rental.rows[0].daysRented)
+        }else{
+            delayFee = 0;
+        }
+
+        await db.query(`UPDATE rentals SET "delayFee" = $1, "returnDate" = $2 WHERE id = $3;`,[delayFee, returnDate, id])
+        res.sendStatus(200)
     }catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { postRentals } from "../controllers/rentals.controllers.js";
+import { postRentals, postRentalsReturn } from "../controllers/rentals.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { rentalsSchema } from "../schemas/rentals.schemas.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals",validateSchema(rentalsSchema), postRentals);
+rentalsRouter.post("/rentals/:id/return", postRentalsReturn)
 
 export default rentalsRouter;
